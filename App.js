304d2c4c324b9c5c5a03d714a08a6b711b033879
gaurav4332 <gaurav4332@gmail.com>
2024-03-21 08:27:16 +0530

    Stack Naviogation Setup

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/screens/Login';
 
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+       <NavigationContainer>
+       <Stack.Navigator>
+       <Stack.Screen name="Login" component={Login} />
+       </Stack.Navigator>
+       </NavigationContainer>
     </View>
   )
 }
